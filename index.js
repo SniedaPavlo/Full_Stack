b011e09d5736569a2e65b557a6d05bd4a5231f56
Pavlo <sniedalx@gmail.com>
2023-02-12 11:44:02 -0800

    делаем авторизацию без БД + JWT token

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from 'express'
-
+import jwt from 'jsonwebtoken'
 //создание сервера с помощью библиотеки
 const app = express()
+
+//используем логику json с самого експресса, чтобы могло читать json
+//по другому будет undefined рпи отправке в json формате на сервер
+app.use(express.json())
 
 //ответ клиенту в случае запроса на главный путь
 app.get('/', (req, res) => {
     res.send('6 Hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+    //при приходе запроса генерируем токен и передаем информацию которую будем шифровать
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Pavlo'
+    }, 'secretPPP')
+
+    // отправляем обьект статуса и сам токен клиенту
+    req.json({
+        seccess: true,
+        token
+    })
 })
 
 //запускаем приложения на указаный нами порт и сообщаем об успехе запуска
@@ -17,4 +35,6 @@ app.listen(4858, (err) => {
 })
 
  //после этого запускаем веб сервер и можем открыть его на порту 4858
+
+
 
